c2cc003ab4bcba6a6bc98fcd5179c4e391adf95d
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-08-27 14:08:32 +0545

    update and delete user actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
-Rails.application.routes.draw do  
-
+Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-  namespace :api, defaults: {format: :json} do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: %i[show create]
+      resources :users, only: %i[show create update destroy]
     end
   end
 end
-
