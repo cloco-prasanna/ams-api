13f8f9a8341484fc19b9151b879116bf3d299cbb
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-08-27 11:58:21 +0545

    user model with validations

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
   namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+    end
   end
 
 end
