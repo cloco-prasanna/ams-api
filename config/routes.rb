e519c74b06adaec45430307d584253f29a677e24
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-08-27 13:49:24 +0545

    create user endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :users, only: [:show]
+      resources :users, only: %i[show create]
     end
   end
 end
+
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -9,8 +9,21 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   get api_v1_user_url(@user), as: :json
   assert_response :success
   json_response = JSON.parse(self.response.body)
-  assert_equal @user.email, json_response['email']
+  assert_equal @user.email, json_response["email"]
  end
+
+ test "should create user" do
+  assert_difference("User.count") do
+  post api_v1_users_url, params: { user: { email:
+  "test@test.org", password: "123456" } }, as: :json
+  end
+  assert_response :created
+  end
+  test "should not create user with taken email" do
+  assert_no_difference("User.count") do
+  post api_v1_users_url, params: { user: { email: @user
+  .email, password: "123456" } }, as: :json
+  end
+  assert_response :unprocessable_entity
+  end
 end
-
-
