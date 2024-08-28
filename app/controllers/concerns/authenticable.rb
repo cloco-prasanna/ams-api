63d553f124c41d49b806a6732c70ff1dbb7b5c35
Prasanna Raj Karki <63549402+prasannakarki77@users.noreply.github.com>
2024-08-28 14:13:04 +0545

    headers token decode issue fix

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -2,10 +2,11 @@ module Authenticable
   def current_user
     return @current_user if @current_user
 
-    header = request.headers["Authorization"]
+    header = request.headers["Authorization"].split(" ").last
     return nil if header.nil?
 
     decoded = JsonWebToken.decode(header)
+
 
     @current_user = User.find(decoded[:user_id]) rescue ActiveRecord::RecordNotFound
   end
